e33abfc8e6104d3e42631a38e085b8a402f4dbdf
Medvediev Yevhen <medvedev.geka@gmail.com>
2023-02-01 22:36:53 +0200

    feat: test passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+	const arr = bracketsConfig.map((el) => {
+		return el.join('');
+	});
+	
+	while (arr.some((el) => str.includes(el))) {
+		arr.forEach((el) => str = str.replaceAll(el, ''));
+	}
+	return !!!str;
+	}
